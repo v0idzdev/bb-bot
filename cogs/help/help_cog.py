e6f0afb7fde72c009d2d47f2b45d45865a55701b
Matthew Flegg <matthewflegg@outlook.com>
2022-03-21 18:15:13 +0000

    Re-added the old ~help command as ~docs

--- cogs/help/help_cog.py
+++ cogs/help/help_cog.py
@@ -2,6 +2,9 @@
 Contains a embedded help command.
 """
 
+import nextcord
+import start
+
 from nextcord.ext import commands
 from .help_command import HelpCommand
 
@@ -17,6 +20,39 @@ class HelpCommandCog(commands.Cog, name='Help'):
     def cog_unload(self):
         """Called when the cog is removed."""
         self.client.help_command = self._current_help_command
+
+    @commands.command()
+    async def docs(self, ctx: commands.Context):
+        """
+        Sends a link to the documentation.
+
+        Usage:
+        ```
+        ~docs
+        ```
+        """
+        embed = nextcord.Embed(
+            title="Beep Boop Bot Documentation",
+            color=start.colour,
+            url="https://github.com/matthewflegg/beepboop/blob/main/README.md",
+            description="View the official commands list for Beep Boop Bot on GitHub.",
+        )
+
+        embed.set_author(
+            name="Matthew Flegg",
+            url="https://github.com/matthewflegg",
+            icon_url="https://imagemagick.org/image/convex-hull.png",
+        )
+
+        embed.set_thumbnail(
+            url="https://media.istockphoto.com/vectors/robot-avatar-icon-vector-id908807494?k=20&m=908807494&s=612x612&w=0&h=N050SIC8pgzsf_LaJT-ZyEE6HHMXLU5PYfMpixuinas="
+        )
+
+        embed.set_footer(
+            text="Contribute to the open source Beep Boop Bot GitHub repository."
+        )
+
+        await ctx.send(embed=embed)
 
 
 def setup(client: commands.Bot):
