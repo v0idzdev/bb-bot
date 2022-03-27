416dc392988a57c535e2014f85bacde7f190e932
Matthew Flegg <matthewflegg@outlook.com>
2022-03-27 22:09:33 +0100

    Commented out the Twitch command

--- cogs/misc/misc_cog.py
+++ cogs/misc/misc_cog.py
@@ -19,55 +19,55 @@ class MiscCog(commands.Cog, name="Misc"):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @commands.command()
-    async def twitch(self, ctx: commands.Context, *, name: str):
-        """
-        🎲 Shows information about a Twitch streamer.
+    # @commands.command()
+    # async def twitch(self, ctx: commands.Context, *, name: str):
+    #     """
+    #     🎲 Shows information about a Twitch streamer.
 
-        Usage:
-        ```
-        ~twitch <streamer name>
-        ```
-        """
-        client = self.client.twitch
-        name = name.lower()
-        broadcaster_data = await client.connect("helix/users", login=name)
-        broad_list = broadcaster_data['data']
+    #     Usage:
+    #     ```
+    #     ~twitch <streamer name>
+    #     ```
+    #     """
+    #     client = self.client.twitch
+    #     name = name.lower()
+    #     broadcaster_data = await client.connect("helix/users", login=name)
+    #     broad_list = broadcaster_data['data']
 
-        if broad_list:
-            broadcaster_id = broad_list[0]['id']
+    #     if broad_list:
+    #         broadcaster_id = broad_list[0]['id']
 
-            broadcaster_name = broad_list[0]['display_name']
-            json = await client.connect('helix/streams', user_id=str(broadcaster_id))
+    #         broadcaster_name = broad_list[0]['display_name']
+    #         json = await client.connect('helix/streams', user_id=str(broadcaster_id))
 
-            if not json['data']:
-                return await ctx.send(f"{broadcaster_name} isn't live")
+    #         if not json['data']:
+    #             return await ctx.send(f"{broadcaster_name} isn't live")
 
-            stream: TwitchBroadcast = await client.return_information(json)
-            stream.thumbnail.seek(0)
-            file = discord.File(fp=stream.thumbnail, filename="stream.png")
+    #         stream: TwitchBroadcast = await client.return_information(json)
+    #         stream.thumbnail.seek(0)
+    #         file = discord.File(fp=stream.thumbnail, filename="stream.png")
 
-            game_cover = stream.game_image
-            game_name = stream.game_name
-            view_count = stream.viewer_count
-            username = stream.username
-            stream_title = stream.stream_title
-            started_at = stream.started_at
+    #         game_cover = stream.game_image
+    #         game_name = stream.game_name
+    #         view_count = stream.viewer_count
+    #         username = stream.username
+    #         stream_title = stream.stream_title
+    #         started_at = stream.started_at
 
-            on_going_for = humanize.precisedelta(datetime.datetime.utcnow() - started_at, format="%0.0f")
+    #         on_going_for = humanize.precisedelta(datetime.datetime.utcnow() - started_at, format="%0.0f")
 
-            embed = discord.Embed(title=stream_title, color=self.client.theme, timestamp=datetime.datetime.utcnow(), url=stream.stream_url)
+    #         embed = discord.Embed(title=stream_title, color=self.client.theme, timestamp=datetime.datetime.utcnow(), url=stream.stream_url)
 
-            embed.set_image(url="attachment://stream.png")
-            embed.set_thumbnail(url=game_cover)
-            embed.add_field(name="Stream Time", value=on_going_for)
-            embed.add_field(name="Username", value=username)
-            embed.add_field(name="Viewer Count", value=view_count)
-            embed.add_field(name="Category", value=game_name)
+    #         embed.set_image(url="attachment://stream.png")
+    #         embed.set_thumbnail(url=game_cover)
+    #         embed.add_field(name="Stream Time", value=on_going_for)
+    #         embed.add_field(name="Username", value=username)
+    #         embed.add_field(name="Viewer Count", value=view_count)
+    #         embed.add_field(name="Category", value=game_name)
 
-            return await ctx.send(embed=embed, file=file)
+    #         return await ctx.send(embed=embed, file=file)
 
-        return await ctx.send(f"{name} isn't a valid streamer's name")
+    #     return await ctx.send(f"{name} isn't a valid streamer's name")
 
     @commands.command()
     async def choose(self, ctx: commands.Context, *choices: str):
