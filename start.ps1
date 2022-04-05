71faf513f487f31b38829033c6865203cf943d51
Matthew Flegg <matthewflegg@outlook.com>
2022-04-05 22:19:52 +0100

    Changed start.bat to start.ps1

---
+++ start.ps1
@@ -0,0 +1,22 @@
+# Install chocolatey, a package manager for Windows.
+# This code is copy-pasted from https://docs.chocolatey.org/en-us/choco/setup.
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Python 3.10.x
+choco install python --version=3.10.0
+
+# Configure project
+py -m pip install virtualenv
+py -m venv venv
+
+# Activating virtual environment
+. venv\Scripts\activate.bat
+
+# Installing dependencies
+pip install -r requirements.txt
+
+# Starting script
+py main.py
